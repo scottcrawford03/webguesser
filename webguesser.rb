260de6b77c3d4b92087cbcfd1a90ea99986e8042
Scott Crawford <scottcrawford03@gmail.com>
2014-12-15 14:13:22 -0700

    initial working game

--- webguesser.rb
+++ webguesser.rb
@@ -4,5 +4,15 @@ require 'sinatra/reloader'
 secret_number = rand(101)
 
 get '/' do
-  erb :index, locals: { :secret_number => secret_number}
+  user_guess = params[:guess].to_i
+  message = if user_guess > secret_number
+              "Your guess of #{user_guess} is too high!"
+            elsif user_guess < secret_number
+              "Your guess of #{user_guess} is too low!"
+            else
+              "Correct!"
+            end
+
+  erb :index, locals: { :secret_number => secret_number,
+                        :message       => message }
 end
