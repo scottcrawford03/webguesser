6c5f669ceb5ecd8c5d227737e996f240c6c2ead0
Scott Crawford <scottcrawford03@gmail.com>
2014-12-15 14:38:51 -0700

    added too high and too low function

--- webguesser.rb
+++ webguesser.rb
@@ -4,15 +4,25 @@ require 'sinatra/reloader'
 secret_number = rand(101)
 
 get '/' do
+  @guess_count = 0
   user_guess = params[:guess].to_i
-  message = if user_guess > secret_number
-              "Your guess of #{user_guess} is too high!"
-            elsif user_guess < secret_number
-              "Your guess of #{user_guess} is too low!"
-            else
-              "Correct!"
-            end
-
+  message = feedback(user_guess, secret_number)
   erb :index, locals: { :secret_number => secret_number,
                         :message       => message }
+end
+
+def feedback(user_guess, secret_number)
+  if user_guess == 0
+    "Take a Guess!"
+  elsif user_guess > secret_number + 5
+    "Your guess of #{user_guess} is too damn high!"
+  elsif user_guess > secret_number
+    "Your guess of #{user_guess} is too high"
+  elsif secret_number > user_guess + 5
+    "Your guess of #{user_guess} is too damn low!"
+  elsif user_guess > secret_number
+    "Your guess of #{user_guess} is too low!"
+  else
+    "Correct! You guessed #{secret_number} and it was right"
+  end
 end
